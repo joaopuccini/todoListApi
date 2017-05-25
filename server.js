fc7474ab4b6c3760644ed8a772fe23e12382715b
renatoppuccini <renatoppuccini@gmail.com>
2017-05-25 15:12:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 8080,
+    port = process.env.PORT || 8081,
     mongoose = require('mongoose'),
     Task = require('./api/models/todoListModel'),
     bodyParser = require('body-parser');
+var assert = require('assert');
+//mongoose.Promise = global.Promise;
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://process.env.MONGODB_DATABASE/Tododb');
-
+//mongoose.connect('mongodb://localhost/Tododb');
+//mongoose.connect('mongodb://mongo:27017/Tododb'
+mongoose.connect('mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':' + process.env.MONGO_PORT_27017_TCP_PORT || '/Tododb', function(err, db) {
+    if (err) {
+        console.error(err);
+        throw err;
+    }
+    assert.equal(null, err);
+    console.log("Connected correctly to server.");
+    //    db.close();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
