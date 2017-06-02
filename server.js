09c6868605a3aba1510b17bea3dae30d87cb5b89
renatoppuccini <renatoppuccini@gmail.com>
2017-06-02 16:33:20 -0300

    server.js openshift connection

--- server.js
+++ server.js
@@ -5,18 +5,17 @@ var express = require('express'),
     Task = require('./api/models/todoListModel'),
     bodyParser = require('body-parser');
 var assert = require('assert');
-//mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
-//mongoose.connect('mongodb://localhost/Tododb');
-//mongoose.connect('mongodb://mongo:27017/Tododb'
-mongoose.connect('mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':' + process.env.MONGO_PORT_27017_TCP_PORT || '/Tododb', function(err, db) {
+//mongoose.connect('mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':' + process.env.MONGO_PORT_27017_TCP_PORT || '/Tododb', function(err, db) {
+mongoose.connect('mongodb://' + process.env.MONGODB_SERVICE_HOST + ':' + process.env.MONGODB_SERVICE_PORT_MONGODB || '/Tododb', function(err, db) {
     if (err) {
         console.error(err);
         throw err;
     }
     assert.equal(null, err);
     console.log("Connected correctly to server.");
-    //    db.close();
+    //db.close();
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
