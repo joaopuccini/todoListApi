d0ce761ce09b5c3d88a5b64b6242552ab0cee3fc
renatoppuccini <renatoppuccini@gmail.com>
2017-06-02 21:21:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,11 @@ mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://redhat:redhat@' + process.env.MONGO_PORT_27017_TCP_ADDR + ':' + process.env.MONGO_PORT_27017_TCP_PORT || '/Tododb', function(err, db) {
 //mongoose.connect('mongodb://' + process.env.DATABASE_SERVICE_NAME +'_MONGODB_USER' +':'+ process.env.DATABASE_SERVICE_NAMEMONGODB_PASSWORD +'@'+  process.env.MONGODB_SERVICE_HOST + ':' + process.env.MONGODB_SERVICE_PORT_MONGODB || '/Tododb', function(err, db) {
  mongoose.connect('mongodb://' +  process.env.MONGODB_SERVICE_HOST + ':' + process.env.MONGODB_SERVICE_PORT_MONGODB || '/Tododb', function(err, db) {
-  //  if (err) {
-   //     console.error(err);
-  //      throw err;
-  //  }
-     if(err){
-        return res.send('err');
-      }
-      return res.send('success');
+   if (err) {
+      console.error(err);
+       throw err;
+   }
+ 
      
     assert.equal(null, err);
     console.log("Connected correctly to server.");
